73e600e576fcea6e788125add65647e9f530c17a
hyperfraise <damien.menigaux@polytechnique.edu>
2026-04-14 19:36:42 +0200

    Add census vintages data definitions and track with DVC

--- src/hexagonal/files/update_specs.py
+++ src/hexagonal/files/update_specs.py
@@ -79,7 +79,7 @@ def update_csv_columns(file: DVCFile, columns: dict[str, ColonneMetadata]):
         with open(file.path, "r", newline="") as fd:
             r = csv.reader(fd)
             header = next(r)
-    except csv.Error, UnicodeDecodeError, FileNotFoundError:
+    except (csv.Error, UnicodeDecodeError, FileNotFoundError):
         return columns
 
     updated_columns = {}
